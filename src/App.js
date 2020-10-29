5099210a1fd0d323abc1b8ed24f059902a77aa30
Juliette Hoisnard <juliette@hoisnard.fr>
2020-10-29 17:27:53 +0100

    get request working in console.log

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 import SearchInput from './components/SearchInput'
+
 
 
 function App() {
   /* TODO: With the help of "useState", you should connect the "SearchInput" field */
-  const [value, setValue] = useState('')
+  const [query, setQuery] = useState('Lord of the Rings')
+  useEffect(() => {
+    // Update the document title using the browser API
+        axios.get('https://api.themoviedb.org/3/search/movie', {
+        params: {
+          api_key: 'dbb2cac3572068ef8027a64c000f9389',
+          query: query
+        },
+      })
+      .then(res => {
+        console.log(res.data)
+        Object.values(res.data.results).forEach(element => {
+          <li>{element['title']}</li>
+        });
+      })},
+  );
+
   return (
     <grid>
       <h1>Juliette's movies</h1>
       <SearchInput
-        value={value}
-        onChange={ event => setValue(event.target.value)}
+        value={query}
+        onChange={ event => setQuery(event.target.value)}
       />
-      <p>{value}</p>
     </grid>
   )
 };
